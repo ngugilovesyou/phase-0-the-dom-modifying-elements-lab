9190c73ec52e32bde442036dab5e8f1e41ba2453
Samuel <amsamgitau@gmail.com>
2024-06-23 16:42:40 +0300

    solution to index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main#main').remove("main")
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Samuel is the champion!";
+newHeader.className = "best";
